0c80859d56110de5d11ba300a950deeb6eb9ddba
Nicholas Carter <nicholas.carter@propcom.co.uk>
2014-10-22 16:16:49 +0100

    [Sitemap] Rejigger visible pages

--- site/templates/sitemap.php
+++ site/templates/sitemap.php
@@ -2,23 +2,21 @@
 	echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-	<? foreach($pages->index() as $p): ?>
-		<? if ($p->visible() == true) : ?>
-			<? if($p->parent() == false) :?>
-				<url>
-					<loc><? echo html($p->url()) ?></loc>
-					<lastmod><? echo $p->modified('c') ?></lastmod>
-					<priority><? echo ($p->isHomePage()) ? 1 : number_format(0.5/$p->depth(), 1) ?></priority>
-				</url>
-				<? if($p->children->_): ?>
-					<? foreach ($p->children->index() as $child): ?>
-						<url>
-							<loc><? echo html($child->url()) ?></loc>
-							<lastmod><? echo $child->modified('c') ?></lastmod>
-							<priority><? echo ($child->isHomePage()) ? 1 : number_format(0.25/$p->depth(), 2) ?></priority>
-						</url>
-					<? endforeach ?>
-				<? endif ?>
+	<? foreach($pages->visible()->index() as $p): ?>
+		<? if($p->parent() == false) :?>
+			<url>
+				<loc><? echo html($p->url()) ?></loc>
+				<lastmod><? echo $p->modified('c') ?></lastmod>
+				<priority><? echo ($p->isHomePage()) ? 1 : number_format(0.5/$p->depth(), 1) ?></priority>
+			</url>
+			<? if($p->children->_): ?>
+				<? foreach ($p->children->index() as $child): ?>
+					<url>
+						<loc><? echo html($child->url()) ?></loc>
+						<lastmod><? echo $child->modified('c') ?></lastmod>
+						<priority><? echo ($child->isHomePage()) ? 1 : number_format(0.25/$p->depth(), 2) ?></priority>
+					</url>
+				<? endforeach ?>
 			<? endif ?>
 		<? endif ?>
 	<? endforeach ?>
