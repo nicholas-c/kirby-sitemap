9c6f021e711a058e8b918b7ca243e41c9cda63d3
Nicholas Carter <nicholas.carter@propcom.co.uk>
2014-10-22 16:31:37 +0100

    [Sitemap] Caps are important

--- site/templates/sitemap.php
+++ site/templates/sitemap.php
@@ -8,7 +8,7 @@
 				<loc><? echo html($p->url()) ?></loc>
 				<lastmod><? echo $p->modified('c') ?></lastmod>
 				<priority><? echo ($p->isHomePage()) ? 1 : number_format(0.5/$p->depth(), 1) ?></priority>
-				<changefreq>Weekly</changefreq>
+				<changefreq>weekly</changefreq>
 			</url>
 			<? if($p->children->_): ?>
 				<? foreach ($p->children->index() as $child): ?>
@@ -17,7 +17,7 @@
 							<loc><? echo html($child->url()) ?></loc>
 							<lastmod><? echo $child->modified('c') ?></lastmod>
 							<priority><? echo ($child->isHomePage()) ? 1 : number_format(0.25/$p->depth(), 2) ?></priority>
-							<changefreq>Weekly</changefreq>
+							<changefreq>weekly</changefreq>
 						</url>
 					<? endif ?>
 				<? endforeach ?>
